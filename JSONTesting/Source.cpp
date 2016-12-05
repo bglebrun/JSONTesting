d72498d138eab4feaade0882dd74262e0dfce79a
7131322 <benjamin.lebrun@mines.sdsmt.edu>
2016-12-05 16:18:55 -0700

    Thing works now

--- JSONTesting/Source.cpp
+++ JSONTesting/Source.cpp
@@ -23,6 +23,10 @@ int main()
 	ofstream fout;
 	json j;
 	json k;
+	string storyTitle;
+	vector <int> storykeys;
+	vector <json> stories;
+	int temp;
 
 	fin.open("ardonianEmbassy.json", ifstream::binary);
 
@@ -34,11 +38,45 @@ int main()
 
 	fin >> j;
 
-	for (json::iterator it = j.begin(); it != j.end(); ++it) {
-		std::cout << it.key() << " : " << it.value() << "\n";
+	storyTitle = j["story"];
+
+	k = j["content"];
+
+	cout << storyTitle << endl << endl;
+
+
+	for (auto i : k) {
+		//cout << i << endl << endl;
+		temp = i["storyKey"];
+		storykeys.push_back(temp);
 	}
 
-	cout << k;
+	cout << "Story keys loaded in order: " << endl;
+
+	for (auto i : storykeys)
+	{
+		cout << i << endl;
+	}
+
+/*	for (json::iterator it = j.begin(); it != j.end(); ++it) {
+		std::cout << it.key() << " : " << it.value() << "\n";
+	}*/
+
+	for (auto i : k)
+	{
+		stories.push_back(i["story"]);
+	}
+
+	for (auto i : stories)
+	{
+		for (auto p : i) {
+			if (p["speaker"] != "")
+				cout << endl << p["speaker"] << ": " << endl;
+
+			cout << endl << p["text"] << endl << endl;
+			system("PAUSE");
+		}
+	}
 
 	return 0;
 
